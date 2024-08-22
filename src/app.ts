68847d63471d78ac5520b5f54880e28b7a1a3f74
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-08-23 01:14:05 +0500

    routes removed

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,9 @@
-import express from "express";
 import "express-async-errors";
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
 
+import { NotFoundError } from "./errors/not-found-error";
 import cookieSession from "cookie-session";
 import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
+import express from "express";
 
 const app = express();
 app.set("trust proxy", true);
@@ -17,13 +13,8 @@ app.use(
   cookieSession({
     signed: false,
     secure: true,
-  })
+  }),
 );
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
