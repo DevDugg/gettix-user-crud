55550577c04fc88578fdc6f7fcb6feb29fe2f69e
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-08-23 01:13:36 +0500

    server

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { app } from "./app";
+
+const start = async () => {
+  if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
+
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("mongo connected");
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on 3000");
+  });
+};
+
+start();
