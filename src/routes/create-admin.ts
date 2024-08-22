14def4c43e37db8c4168762896317261ef5c3523
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-08-23 04:15:13 +0500

    create admin router

---
+++ src/routes/create-admin.ts
@@ -0,0 +1,61 @@
+import { Request, Response, Router } from "express";
+
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../services/password";
+import { PrismaClient } from "@prisma/client";
+import { body } from "express-validator";
+import { validateRequest } from "../middlewares/validate-request";
+
+const router = Router();
+const prisma = new PrismaClient();
+
+router.post(
+  "/api/admin/create",
+  [
+    body("username", "Username is required").notEmpty(),
+    body("password", "Password is required").trim().isLength({ min: 8, max: 20 }),
+    body("role", "Role must be a string").notEmpty().withMessage("Role is required"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { username, password, role } = req.body;
+
+    const existingAdmin = await prisma.admin.findUnique({
+      where: {
+        username,
+      },
+      select: {
+        id: true,
+        username: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    // // throw error if exists
+    if (existingAdmin) throw new BadRequestError("Account already exists");
+    // hash the password
+    const hashedPassword = await Password.toHash(password);
+
+    // create a new admin
+    const newAdmin = await prisma.admin.create({
+      data: {
+        username,
+        password: hashedPassword,
+        role,
+      },
+      select: {
+        id: true,
+        username: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    res.status(201).send(newAdmin);
+  },
+);
+
+export { router as createAdminRouter };
